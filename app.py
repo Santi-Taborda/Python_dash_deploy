193599027f1ab8f14789932e5d0607f230d2046f
Santi-Taborda <84212077+Santi-Taborda@users.noreply.github.com>
2024-06-13 22:06:35 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from dash import Dash, html, dcc, page_registry, page_container
+import dash_bootstrap_components as dbc
+
+app= Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.CERULEAN])
+
+# Define the navigation bar
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("HOME", href="/")),
+    ],
+    brand="SATMA DASHBOARDS",
+    brand_href="/",
+    color="dark",
+    dark=True,
+)
+
+app.layout =html.Div([
+    navbar,
+    page_container
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
