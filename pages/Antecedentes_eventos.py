070c60726b110e296ba5bff557e2a0dd31b84e37
Santi-Taborda <84212077+Santi-Taborda@users.noreply.github.com>
2024-08-24 11:27:15 -0500

    Update Antecedentes_eventos.py

--- pages/Antecedentes_eventos.py
+++ pages/Antecedentes_eventos.py
@@ -31,7 +31,7 @@ def datos_iniciales():
 
     return dimestacion, fecha_actual, fecha_40_dias_atras
 
-Puntos, max_actualized, min_actualized = datos_iniciales()
+puntos, max_actualized, min_actualized = datos_iniciales()
 
 register_page(__name__, name="Consulta_antecedentes", path='/SATMA/consulta_antecedentes' )
 
@@ -74,11 +74,17 @@ layout= dbc.Container(children=[
                         value=40,
                         type='number',
                         className='mb-3'),
+                    html.Div(
+                    children=[
+                        dcc.Graph(id='tabla_lluvia_antecedentes'),
+                    ],
+                    style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center'}
+                    ),
                     
 
                     ])], 
                     className="shadow p-3 mb-5 bg-white rounded"
-            )], width=3
+            ), ], width=3
         ),
         dbc.Col(
             dcc.Graph(id='Monitor_consulta_antecedentes'),style={'overflowY': 'scroll', 'height': '100%'},
@@ -89,6 +95,7 @@ layout= dbc.Container(children=[
 
 @callback(
     Output('Monitor_consulta_antecedentes', 'figure'),
+    Output('tabla_lluvia_antecedentes', 'figure'),
     Input('Cantidad_dias_antecedentes', 'value'),
     Input('Latitud_consulta_antecedentes', 'value'),
     Input('Longitud_consulta_antecedentes', 'value'),
@@ -98,14 +105,16 @@ layout= dbc.Container(children=[
 def update_monitor(dias, latitud, longitud, dia_inicio):
     datos_tabla,max_actualized,min_actualized= datos_iniciales()
     fecha=date.fromisoformat(dia_inicio)
+    #fecha=date.today()
     hora_1159pm = time(23, 59, 0)
     max_actualized=datetime.combine(fecha, hora_1159pm)
     min_actualized=max_actualized - timedelta(days=dias)
-
+    #min_actualized=max_actualized - timedelta(days=40)
     puntos = np.empty((len(datos_tabla), 2))
 
     for i, row in datos_tabla.iterrows():
         puntos[i] = [row['Latitud'],row['Longitud']]
+    #nuevo_punto = np.array([4.8146, -75.6981])
     nuevo_punto = np.array([latitud, longitud])
     tree = cKDTree(puntos)
     distancias, indices = tree.query(nuevo_punto, k=5)
@@ -118,9 +127,9 @@ def update_monitor(dias, latitud, longitud, dia_inicio):
     
     query="SELECT idEstacion, date(IdTiempoRegistro) AS IdTiempoRegistro, ROUND(SUM(Valor), 2) AS Valor FROM factmonitoreo WHERE idEstacion IN (%s, %s, %s, %s, %s) AND IdVariable IN (2) AND IdTiempoRegistro BETWEEN %s AND %s AND Valor IS NOT NULL GROUP BY idEstacion, date(IdTiempoRegistro)"
 
-    datos = pd.read_sql(query, engine, params=(datos_tabla_filtrados.iloc[0]['idEstacion'],datos_tabla_filtrados.iloc[1]['idEstacion'],datos_tabla_filtrados.iloc[2]['idEstacion'],datos_tabla_filtrados.iloc[3]['idEstacion'],datos_tabla_filtrados.iloc[4]['idEstacion'],min_actualized, max_actualized))
+    datos_fin = pd.read_sql(query, engine, params=(datos_tabla_filtrados.iloc[0]['idEstacion'],datos_tabla_filtrados.iloc[1]['idEstacion'],datos_tabla_filtrados.iloc[2]['idEstacion'],datos_tabla_filtrados.iloc[3]['idEstacion'],datos_tabla_filtrados.iloc[4]['idEstacion'],min_actualized, max_actualized))
 
-    datos = pd.merge(datos, datos_tabla_filtrados, on="idEstacion")
+    datos_fin = pd.merge(datos_fin, datos_tabla_filtrados, on="idEstacion")
 
     cant_figures=pd.unique(datos_tabla_filtrados["Estacion"])
 
@@ -131,17 +140,24 @@ def update_monitor(dias, latitud, longitud, dia_inicio):
             
         fig.update_layout(autosize=True, title_text="EL PUNTO INTRODUCIDO NO TIENE ESTACIONES CERCANAS")
 
-    else: 
+    else:
+        sumas = np.empty((5, 1))
         fig= make_subplots(rows=len(cant_figures), cols=1, subplot_titles=cant_figures, vertical_spacing=0.1)
         for index, figure in enumerate(cant_figures):
-            datos= datos[datos['Estacion']==figure]
+            datos= datos_fin[datos_fin['Estacion']==figure]
             suma=0
+            distancia=round(distancias[index],3)
             for indexer, row in datos.iterrows():
-                suma=suma+row["Valor"]
+                if pd.isnull(datos.loc[indexer,'Valor']):
+                    suma=suma
+                else:
+                    suma=suma+row["Valor"]
+
                 datos.at[indexer, 'Valor']=suma
             suma=round(suma,2)
+            sumas[index]=suma
 
-            text=""+figure+":\n Lluvia acumulada: "+str(suma)+" mm"
+            text=""+figure+""
             fig.add_trace({
                 'type': 'scatter',
                 'x': datos['IdTiempoRegistro'],
@@ -153,5 +169,23 @@ def update_monitor(dias, latitud, longitud, dia_inicio):
             row=index+1, col=1)
             fig.update_yaxes(title_text="Lluvia acumulada (mm)", row=index+1, col=1)
 
-        fig.update_layout(autosize=True, height=len(cant_figures)*300, bargap=0.1, plot_bgcolor="white", paper_bgcolor="LightSteelBlue", margin=dict(l=30, r=30, t=30, b=30))
-    return fig
+        fig.update_layout(autosize=True, height=len(cant_figures)*300, bargap=0.1, plot_bgcolor="white", paper_bgcolor="LightSteelBlue", margin=dict(l=30, r=30, t=30, b=0))
+    
+    fig_4= go.Figure(data=[go.Table(
+    header=dict(values=['Estación', 'Precipitación acumulada', 'Distancia'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+    cells=dict(values=[[datos_tabla_filtrados.iloc[0]['Estacion'], datos_tabla_filtrados.iloc[1]['Estacion'], datos_tabla_filtrados.iloc[2]['Estacion'], datos_tabla_filtrados.iloc[3]['Estacion'], datos_tabla_filtrados.iloc[4]['Estacion']],
+                       [sumas[0], sumas[1], sumas[2],sumas[3],sumas[4]],
+                       [round(distancias[0]*111,3),round(distancias[1]*111,3),round(distancias[2]*111,3),round(distancias[3]*111,3),round(distancias[4]*111,3)]],
+                       
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+    ])
+
+    fig_4.update_layout(width=300, height=300, margin=dict(l=0, r=0, t=20, b=10))
+
+
+    return fig, fig_4
