46c5b4d74d4bf82e8f55897c19f9f2bd328e2951
Santi-Taborda <84212077+Santi-Taborda@users.noreply.github.com>
2024-10-28 13:27:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from dash import Dash, html, dcc, page_registry, page_container
 import dash_bootstrap_components as dbc
 
 app= Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.CERULEAN])
-server=app.server
 
 # Define the navigation bar
-navbar = dbc.NavbarSimple(
+""" navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("HOME", href="/")),
     ],
@@ -13,11 +12,12 @@ navbar = dbc.NavbarSimple(
     brand_href="/",
     color="dark",
     dark=True,
-)
+) """
 
 app.layout =html.Div([
-    navbar,
+    #navbar,
     page_container
 ])
+
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
