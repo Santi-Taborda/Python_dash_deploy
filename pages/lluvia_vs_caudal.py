83b3371a1d1ed9135c5f1abe471b43caa86e5c89
Santi-Taborda <84212077+Santi-Taborda@users.noreply.github.com>
2024-11-26 11:11:12 -0500

    Update lluvia_vs_caudal.py

--- pages/lluvia_vs_caudal.py
+++ pages/lluvia_vs_caudal.py
@@ -18,39 +18,64 @@ env['DB_URL']="mysql+pymysql://{user}:{password}@{host}:{port}/{name}".format(
     name=env['DB_NAME']
     )
 
-def redondeo_cincominutal(datetime_datos_tabla):
-    # Encuentra la cantidad de minutos desde el inicio de la hora
-    minutos = (datetime_datos_tabla - datetime_datos_tabla.replace(minute=0, second=0, microsecond=0)).total_seconds() / 60
-    # Encuentra el múltiplo de 5 más cercano
-    minutos_redondeados = int(np.round(minutos / 5.0) * 5)
-    # Calcula la hora redondeada
-    datetime_redondeado = datetime_datos_tabla.replace(minute=0, second=0, microsecond=0) + timedelta(minutes=minutos_redondeados)
-    return datetime_redondeado
-
+def calcular_valor(row):
+        if row['idEstacion']==8:
+            return row['Valor']*47.1844
+        elif row['idEstacion']==9:
+            return row['Valor']*21.2714
+        elif row['idEstacion']==10:
+            return row['Valor']*19.505
+        elif row['idEstacion']==16:
+            return row['Valor']*38.4229
+        elif row['idEstacion']==21:
+            return row['Valor']*4.7685
+        elif row['idEstacion']==22:
+            return row['Valor']*12.1287
+        elif row['idEstacion']==23:
+            return row['Valor']*34.2024
+        elif row['idEstacion']==24:
+            return row['Valor']*29.8929
+        elif row['idEstacion']==26:
+            return row['Valor']*4.013
+        elif row['idEstacion']==79:
+            return row['Valor']*9.6955
+        elif row['idEstacion']==80:
+            return row['Valor']*23.3734
+        elif row['idEstacion']==122:
+            return row['Valor']*25.6947
 
 def obtener_datos():
     fecha_actual= datetime.now().replace(tzinfo=pd.Timestamp.now().tz)
     fecha_40_dias_atras=fecha_actual - timedelta(days=3)
     hora_actual_str = fecha_actual.strftime('%Y-%m-%d %H:%M:%S')
     hace_40_dias_str = fecha_40_dias_atras.strftime('%Y-%m-%d %H:%M:%S')
-# Conexión a la base de datos MySQL
+    # Conexión a la base de datos MySQL
     engine = create_engine(env.get('DB_URL'), echo=True)
     # Consultas SQL
+    area_total=321.4674
     
-    query1 = "SELECT idVariable, FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(IdTiempoRegistro) / 300) * 300) AS IdTiempoRegistro, round(SUM(Valor), 2) AS Valor FROM factmonitoreo WHERE idEstacion IN (14, 31, 26, 84, 24, 25, 79, 23, 10, 15, 8, 3) AND IdVariable in (2) AND IdTiempoRegistro BETWEEN %s AND %s AND Valor IS NOT NULL GROUP BY FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(IdTiempoRegistro) / 300) * 300) "
+    query1 = "SELECT idVariable, idEstacion, FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(IdTiempoRegistro) / 300) * 300) AS IdTiempoRegistro, round(SUM(Valor), 2) AS Valor FROM factmonitoreo WHERE idEstacion IN (8, 9, 10, 16, 21, 22, 23, 24, 26, 79, 80, 122) AND IdVariable in (2) AND IdTiempoRegistro BETWEEN %s AND %s AND Valor IS NOT NULL GROUP BY FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(IdTiempoRegistro) / 300) * 300), idEstacion"
     query2 = "SELECT idVariable, FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(IdTiempoRegistro) / 300) * 300) AS IdTiempoRegistro, round(SUM(Valor), 2) AS Valor FROM factmonitoreo WHERE idEstacion IN (84,31) AND IdVariable in (12) AND IdTiempoRegistro BETWEEN %s AND %s AND Valor IS NOT NULL GROUP BY FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(IdTiempoRegistro) / 300) * 300) "
     datos_tabla_1 = pd.read_sql(query1, engine,  params=(hace_40_dias_str, hora_actual_str))
     datos_tabla_2 = pd.read_sql(query2, engine,  params=(hace_40_dias_str, hora_actual_str))
+    datos_tabla_1["Valor_ponderado"]=np.nan
+    
+    datos_tabla_1['Valor_ponderado'] = datos_tabla_1.apply(calcular_valor, axis=1)
+
+
+    datos_tabla_lluvia = datos_tabla_1.groupby("IdTiempoRegistro", as_index=False)["Valor_ponderado"].sum()
+    datos_tabla_lluvia["Valor_ponderado"]=datos_tabla_lluvia["Valor_ponderado"]/area_total
+    datos_tabla_lluvia.rename(columns={'Valor_ponderado': 'Valor'}, inplace=True)
 
     #Concateno tablas
-    datos_tabla = pd.merge(datos_tabla_1, datos_tabla_2, on="IdTiempoRegistro")
+    datos_tabla = pd.merge(datos_tabla_lluvia, datos_tabla_2, on="IdTiempoRegistro")
     #Redondeo a 5 minutos
     datos_tabla["IdTiempoRegistro"] = pd.to_datetime(datos_tabla["IdTiempoRegistro"], utc=True)
-    datos_tabla['IdTiempoRegistro'] = datos_tabla['IdTiempoRegistro'].apply(redondeo_cincominutal)
     datos_tabla["timestamp"] = datos_tabla["IdTiempoRegistro"].astype('int64') // 10**9
     datos_tabla.sort_values(by="IdTiempoRegistro", inplace=True)
     time_data_min=datos_tabla["timestamp"].iloc[0]
     time_data_max=datos_tabla["timestamp"].iloc[-1]
+
 
     return (datos_tabla, time_data_min, time_data_max)
 
