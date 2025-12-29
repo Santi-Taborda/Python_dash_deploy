a23363f81fc5fc739dbb96ef564f3c4a56a746c9
Santi-Taborda <84212077+Santi-Taborda@users.noreply.github.com>
2025-12-29 10:59:20 -0500

    Actualizar SAT_Bomberos_Capacitacion.py

--- pages/SAT_Bomberos_Capacitacion.py
+++ pages/SAT_Bomberos_Capacitacion.py
@@ -1,3 +1,4 @@
+"""
 from dash import Dash, html, dcc, callback, Output, Input, register_page
 import dash_bootstrap_components as dbc
 import plotly.express as px
@@ -191,4 +192,4 @@ def update_map(_, n):
         markers.append(marker)
 
     return markers
-
+"""
