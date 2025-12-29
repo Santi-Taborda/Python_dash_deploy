6e21a5b8aff65cba960b9c40ad4cc55d6e0ac09f
Santi-Taborda <84212077+Santi-Taborda@users.noreply.github.com>
2025-12-29 10:22:41 -0500

    Actualizar Analisis_El_Lago.py

--- pages/Analisis_El_Lago.py
+++ pages/Analisis_El_Lago.py
@@ -8,11 +8,6 @@ from os import environ as env
 from sqlalchemy import create_engine
 import pymysql
 
-env['DB_USER']='utpmon'
-env['DB_PASSWORD']='UtpM0n1t0r'
-env['DB_HOST']='194.163.137.37'
-env['DB_PORT']='3306'
-env['DB_NAME']='upt_monestaciones'
 
 env['DB_URL']="mysql+pymysql://{user}:{password}@{host}:{port}/{name}".format(
     user=env['DB_USER'],
